a7bdefa6c452456582ad69cf894276c716841cbb
macotosama <jtn1999@outlook.com>
2021-01-13 21:23:31 -0600

    Update

--- ClienteFrontend/src/app/app-routing.module.ts
+++ ClienteFrontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './interfaz/home/home.component';
 import { ActivadadesComponent } from './interfaz/activadades/activadades.component';
 import { HabitacionesComponent } from './interfaz/habitaciones/habitaciones.component';
+import { PagoHabitacionComponent } from './interfaz/pago-habitacion/pago-habitacion.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent },
   {path: 'actividades', component: ActivadadesComponent },
   {path: 'habitaciones', component: HabitacionesComponent },
+  {path: 'pagoHabitacion', component: PagoHabitacionComponent },
 ];
 
 @NgModule({
